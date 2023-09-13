cdd875c2537b0f903228540c28312face2282ba3
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-13 22:57:18 +0300

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (isNaN(num1) || 
+        isNaN(num2) || 
+        num1 < 0 || 
+        num2 < 0 ||
+        typeof num1 === "string" ||
+        typeof num2 === "string") {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
+
 
 };
 

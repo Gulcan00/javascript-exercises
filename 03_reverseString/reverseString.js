75ee1aa00ddf47380146ef37ef7c40ae2ae1a513
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-13 22:38:44 +0300

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const words = string.split(" ");
+    const reverse = [];
 
+    for (let word of words) {
+        //Split word into array of characters
+        letters = word.split("");
+
+        //Reverse the characters then join add to array
+        reverse.push(letters.reverse().join(""));
+    }
+
+    return reverse.reverse().join(" ");
 };
 
 // Do not edit below this line

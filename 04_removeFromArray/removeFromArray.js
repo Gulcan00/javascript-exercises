492cc71e249100dc1cba13c2b25784653e4be335
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-13 22:50:31 +0300

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let num of args) {
+        const index = array.indexOf(num);
+        if (index !== -1) { //If value is present in array
+            if (index === 0) {
+                array.shift();
+            } else if (index === array.length -1) {
+                array.pop();
+            } else {
+                array = array.slice(0, index).concat(array.slice(index + 1));
+            }
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line

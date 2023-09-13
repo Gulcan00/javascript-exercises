e441a94c40df04497cb5f9721803aaa301ff9594
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-13 22:26:45 +0300

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    if (count === 0) {
+        return "";
+    }
 
+    if (count < 0) {
+        return "ERROR";
+    }
+
+    const temp = string;
+    for(let i = 0; i < count-1; i++) {
+        string += temp;
+    }
+
+    return string;
 };
 
 // Do not edit below this line

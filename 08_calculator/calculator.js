40a73c34e8f61acd70df2c5cbe9fa0378048819c
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-17 19:38:10 +0300

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, item) => mul * item, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	 let power = 1;
+   for(let i = 0; i < num2; i++) {
+     power *= num1;
+   }
+
+   return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fac = 1;
+  for(let i = 2; i <= num; i++)
+    fac *= i;
+
+  return fac;
 };
 
 // Do not edit below this line

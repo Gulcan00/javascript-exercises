ee32698390962b0ade47be7d9f88f1e6694b121e
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-17 20:05:44 +0300

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+   if (num < 0)
+    return 'OOPS';
 
+   let first = 1;
+   let next = 1;
+   let f = 1;
+
+   for (let i = 2; i < num; i++) 
+    {
+        f = first + next;
+        first = next;
+        next = f;
+    }
+   return f;
 };
 
 // Do not edit below this line

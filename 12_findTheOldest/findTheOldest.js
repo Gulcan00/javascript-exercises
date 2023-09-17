04d79f2bacc790b85826df4d0d1d33e6cc464a26
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-17 20:30:34 +0300

    Alt solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,22 @@
 const findTheOldest = function(arr) {
-    const ages = arr.map((person) => {
-     return {
-            name: person.name,
-            age: person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (new Date().getFullYear() - person.yearOfBirth)
-    }});
+    // const ages = arr.map((person) => {
+    //  return {
+    //         name: person.name,
+    //         age: person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (new Date().getFullYear() - person.yearOfBirth)
+    // }});
 
-    ages.sort((a, b) => b.age - a.age);
-    return ages[0];
+    // ages.sort((a, b) => b.age - a.age);
+    // return ages[0];
+
+    return arr.reduce((obj, person) => {
+        const age = person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (new Date().getFullYear() - person.yearOfBirth);
+        
+        if (age > obj.age) {
+            obj.age = age;
+            obj.name = person.name;
+        }
+        return obj;
+    }, {age: 0, name: ''});
 };
 
 // Do not edit below this line

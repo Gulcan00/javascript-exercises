9a41fb4c4d46ab55129568aeaa03fb390a9fda47
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-17 19:56:10 +0300

    Complete 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[.!, ]/g, "");
+    let rev = str.split("").reverse();
+    str = str.split("")
+    if (str.length === rev.length) {
+        return str.every((letter, index) => letter === rev[index]);
+    }
+    return false;
 };
 
 // Do not edit below this line

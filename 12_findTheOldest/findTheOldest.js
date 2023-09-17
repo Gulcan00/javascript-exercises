46efb7eb1c91024f764b6a741ea5022b2e2d42a7
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-17 20:24:16 +0300

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const ages = arr.map((person) => {
+     return {
+            name: person.name,
+            age: person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (new Date().getFullYear() - person.yearOfBirth)
+    }});
 
+    ages.sort((a, b) => b.age - a.age);
+    return ages[0];
 };
 
 // Do not edit below this line
